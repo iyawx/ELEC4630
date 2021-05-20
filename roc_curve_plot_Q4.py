e2360a8be570280d321ce8e2faa9a91742ed8790
iyawx <79889031+iyawx@users.noreply.github.com>
2021-05-20 17:04:23 +1000

    Add files via upload

--- roc_curve_plot_Q4.py
+++ roc_curve_plot_Q4.py
@@ -20,23 +20,26 @@ known_faces = [
 
 # Load an image with an unknown face
 for img in glob.iglob("*.jpg"):
-    unknown_image = face_recognition.load_image_file(img)
+    try:
+        unknown_image = face_recognition.load_image_file(img)
 
-    # Find all the faces and face encodings in the unknown image
-    unknown_face_encoding = face_recognition.face_encodings(unknown_image)[0]
+        # Find all the faces and face encodings in the unknown image
+        unknown_face_encoding = face_recognition.face_encodings(unknown_image)[0]
 
-    # See if the face is a match for the known face(s)
-    matches = face_recognition.compare_faces(known_faces, unknown_face_encoding,0.6)
+        # See if the face is a match for the known face(s)
+        matches = face_recognition.compare_faces(known_faces, unknown_face_encoding,0.55)
 
-    # Or instead, use the known face with the smallest distance to the new face
-    face_distances = face_recognition.face_distance(known_faces, unknown_face_encoding)
+        # Or instead, use the known face with the smallest distance to the new face
+        face_distances = face_recognition.face_distance(known_faces, unknown_face_encoding)
 
-    if img == "Me.jpg" or img == "unknown_test10.jpg" or img == "unknown_test9.jpg":
-        actual = np.block([actual, 0])
-        predictions = np.block([predictions, face_distances])
-    else:
-        actual = np.block([actual, 1])
-        predictions = np.block([predictions, face_distances])
+        if img == "Me_1.jpg" or img == "Me_2.jpg" or img == "Me_3.jpg" or img == "Me_4.jpg" or img == "Me_5.jpg" or img == "Me_6.jpg" or img == "Me_7.jpg":
+            actual = np.block([actual, 1])
+            predictions = np.block([predictions, 1-face_distances])
+        else:
+            actual = np.block([actual, 0])
+            predictions = np.block([predictions, 1-face_distances])
+    except IndexError:
+        print("I wasn't able to locate any faces in {}".format(img))
 
 false_positive_rate, true_positive_rate, thresholds = roc_curve(actual, predictions)
 roc_auc = auc(false_positive_rate, true_positive_rate)
